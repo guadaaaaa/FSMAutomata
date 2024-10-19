ec1f508f6cded34347b030ee9e81f894df634132
guadaaaaa <obandoguadalue951@gmail.com>
2024-10-19 22:57:59 +0800

    working FSM - BombParty Inspired with 5 Seconds Timer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Main {
 
@@ -9,32 +10,42 @@ public class Main {
         for(int i = 0; i<checkStr.length(); i++){
             if(currState == 0){
                 if(checkStr.charAt(i) == states[state]){
+                    System.out.println("Current State 0  to State 1" + checkStr.charAt(i) + " "+ states[state]);
                     currState++;
                     state++;
                 } else {
+                    System.out.println("Current State 0 to State 0"+ checkStr.charAt(i) + " "+ states[state]);
+                    currState = 0;
                     state = 0;
+                    if(checkStr.charAt(i) == states[0]) i--;
                     if (i + 1 >= checkStr.length()) {
                         break;
                     }
                 }
             } else if(currState == 1){
                 if(checkStr.charAt(i) == states[state]){
+                    System.out.println("Current State 1 to State 2"+ checkStr.charAt(i) + " "+ states[state]);
                     currState++;
                     state++;
                 } else {
+                    System.out.println("Current State 1 to State 0"+ checkStr.charAt(i) + " "+ states[state]);
                     currState = 0;
                     state = 0;
+                    if(checkStr.charAt(i) == states[0]) i--;
                     if (i + 1 >= checkStr.length()) {
                         break;
                     }
                 }
             } else if(currState == 2){
                 if(checkStr.charAt(i) == states[state]){
+                    System.out.println("Current State 2 to State 3"+ checkStr.charAt(i) + " "+ states[state]);
                     currState++;
                     break;
                 } else {
+                    System.out.println("Current State 2 State 0"+ checkStr.charAt(i) + " "+ states[state]);
                     currState = 0;
                     state = 0;
+                    if(checkStr.charAt(i) == states[0]) i--;
                     if (i + 1 >= checkStr.length()) {
                         break;
                     }
@@ -51,24 +62,49 @@ public class Main {
         String[] strs = {"ses", "ath", "hum", "que", "com", "ack", "eed", "ing", "ick", "min"};
         int[] life = {2,2};
         int currPlayer = 0;
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+
         do {
             String str = strs[rand.nextInt(strs.length)];
             System.out.println("Word that contains " + str);
-            System.out.println("Player " + (currPlayer + 1) + ": ");
-            String checkStr = sc.nextLine();
-            answer = checkString(str, checkStr);
-            if (answer) {
-                if (currPlayer == 1) {
-                    currPlayer = 0;
-                    System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
-                    System.out.println("Player " + (currPlayer + 1) + "'s turn ");
+            System.out.println("Player " + (currPlayer + 1) + ": You have 5 seconds to answer!");
+            Future<String> futureInput = executor.submit(() -> sc.nextLine());
+            try {
+                String checkStr = futureInput.get(5, TimeUnit.SECONDS);
+                answer = checkString(str, checkStr);
+                if (answer) {
+                    if (currPlayer == 1) {
+                        currPlayer = 0;
+                        System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
+                        System.out.println("Player " + (currPlayer + 1) + "'s turn ");
+                    } else {
+                        currPlayer = 1;
+                        System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
+                        System.out.println("Player " + (currPlayer + 1) + "'s turn ");
+                    }
                 } else {
-                    currPlayer = 1;
-                    System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
-                    System.out.println("Player " + (currPlayer + 1) + "'s turn ");
+                    life[currPlayer] = life[currPlayer] - 1;
+                    if(life[currPlayer] != 0){
+                        if (currPlayer == 1) {
+                            currPlayer = 0;
+                            System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
+                            System.out.println("Player " + (currPlayer + 1) + "'s turn ");
+                        } else {
+                            currPlayer = 1;
+                            System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
+                            System.out.println("Player " + (currPlayer + 1) + "'s turn ");
+                        }
+                    } else {
+                        break;
+                    }
                 }
-            } else {
-                life[currPlayer] = life[currPlayer] - 1;
+            } catch (TimeoutException e) {
+                // If the player takes too long, skip their turn
+                System.out.println("Time's up! Player " + (currPlayer + 1) + " loses a life.");
+                life[currPlayer]--;
                 if(life[currPlayer] != 0){
                     if (currPlayer == 1) {
                         currPlayer = 0;
@@ -82,7 +118,11 @@ public class Main {
                 } else {
                     break;
                 }
+                futureInput.cancel(true);
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
             }
+
         } while (true);
 
         if(life[0] == 0){
@@ -92,5 +132,8 @@ public class Main {
             System.out.println("Score Update\nPlayer 1: " + life[0] + "\nPlayer 2: "+ life[1]);
             System.out.println("Player 1 wins!");
         }
+
+        executor.shutdown();
+        scheduler.shutdown();
     }
 }
